deaf06f175a7e04d03a8b4b96b65b7b2673db6cd
Halx <alexandre.barbez@gmail.com>
2018-05-28 20:42:31 +0200

    Using Spring tools to manipulate Request and parameters inside it. Not as usefull as ModelAttribute but nice to know them :)

--- src/main/java/com/halx/springmvc/HomeController.java
+++ src/main/java/com/halx/springmvc/HomeController.java
@@ -3,9 +3,13 @@ package com.halx.springmvc;
 import com.halx.spring.utils.Utils;
 import com.halx.springmvc.model.Student;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+
 
 @Controller
 public class HomeController {
@@ -22,15 +26,21 @@ public class HomeController {
     @GetMapping("/loginForm")
     public String showForm(Model model){
         model.addAttribute("student",student);
+
         System.out.println(Utils.getDateFormattedLikeSpringBoot()+" - HomeController => forwarding request to loginForm template");
         return "loginForm";
     }
 
     @PostMapping("/loginForm")
-    public String processForm(@ModelAttribute Student student){
-        System.out.println(Utils.getDateFormattedLikeSpringBoot()+" - HomeController => forwarding request to helloWorld template");
+    public String processForm(HttpServletRequest request,@RequestParam("name") String name ,@ModelAttribute Student student){
+
+        student.setName(student.getName().toUpperCase());
+
+        System.out.println(Utils.getDateFormattedLikeSpringBoot()+" - HomeController => Manipulating the request multiples ways :");
+        System.out.println(Utils.getDateFormattedLikeSpringBoot()+" - HomeController => HttpServletRequest.getParameter : "+request.getParameter("name"));
+        System.out.println(Utils.getDateFormattedLikeSpringBoot()+" - HomeController => @RequestParam : "+name);
+        System.out.println(Utils.getDateFormattedLikeSpringBoot()+" - HomeController => @ModelAttribute : "+student.getName());
+
         return "helloWorld";
     }
-
-
 }
