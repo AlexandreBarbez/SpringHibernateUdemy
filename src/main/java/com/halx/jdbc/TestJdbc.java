73567084b3b650a158f4ca6f0c0f98691b3b180f
Alex <alexandre.barbez@gmail.com>
2019-01-07 20:55:45 +0100

    Maj from gitlab

--- src/main/java/com/halx/jdbc/TestJdbc.java
+++ src/main/java/com/halx/jdbc/TestJdbc.java
@@ -2,8 +2,7 @@ package com.halx.jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.util.Properties;
 
 public class TestJdbc {
     public static void main(String[] args) {
@@ -16,14 +15,7 @@ public class TestJdbc {
             Class.forName("org.postgresql.Driver");
             Connection connection = DriverManager.getConnection(jdbcUrl, user, password);
             System.out.println("Connected to the PostGres server successfully opened !");
-
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT version()");
-            while(resultSet.next()) {
-                System.out.println("Information concernant la base de donnée : ");
-                System.out.println(resultSet.getString(1));
-            }
-
+            
             connection.close();
             System.out.println("Connected to the PostGres server successfully closed !");
         }catch(Exception e){
